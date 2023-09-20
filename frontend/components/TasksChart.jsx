978aed831557e410cf151830ce290b832603da26
alrigo123 <alexestrada2804@gmail.com>
2023-09-20 11:44:19 -0500

    TasksForm-DESIGN

--- frontend/components/TasksChart.jsx
+++ frontend/components/TasksChart.jsx
@@ -1,69 +1,39 @@
 import React from 'react'
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import TasksForm from './TasksForm';
 
 const TasksChart = () => {
     const data = [10, 20, 30, 40, 50];
 
     return (
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 
+        <div className="flex flex-col overflow-x-scroll">
             <Link href="/TasksForm">
-                <button
-                    type="button"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    Go to Form
-                </button>
+                <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ADD TASK</button>
             </Link>
-
-
-
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="w-full border-collapse">
+                <thead className="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            ID
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Description
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            at - to
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Status
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Actions
-                        </th>
+                        <th className="px-6 py-3 text-xs font-medium text-gray-700 uppercase tracking-wider">ID</th>
+                        <th className="px-6 py-3 text-xs font-medium text-gray-700 uppercase tracking-wider">Description</th>
+                        <th className="px-6 py-3 text-xs font-medium text-gray-700 uppercase tracking-wider">at - to</th>
+                        <th className="px-6 py-3 text-xs font-medium text-gray-700 uppercase tracking-wider">Status</th>
+                        <th className="px-6 py-3 text-xs font-medium text-gray-700 uppercase tracking-wider">Actions</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            1
-                        </th>
-                        <td class="px-6 py-4">
-                            Tarea de topicos  hacer el mapa
+                <tbody className="bg-white">
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap">1</td>
+                        <td className="px-6 py-4 whitespace-nowrap">Tarea de topicos  hacer el mapa</td>
+                        <td className="px-6 py-4 whitespace-nowrap">10:23 - 11:45</td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <button type="button" className="bg-green-700 hover:bg-green-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">DONE</button>
+                            <button type="button" className="bg-red-700 hover:bg-red-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Red</button>
                         </td>
-                        <td class="px-6 py-4">
-                            10:23 - 11:45
-                        </td>
-                        <td class="px-6 py-4">
-                            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                DONE (CLICKEABLE PARA CAMBIAR ESTADO)
-                            </button>
-                            <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
-
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-red-800 dark:text-red-500 hover:underline mx-2">Delete</a>
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <a href="#" className="text-red-800 hover:underline">Delete</a>
+                            <a href="#" className="text-blue-600 hover:underline">Edit</a>
                         </td>
                     </tr>
-
                 </tbody>
             </table>
         </div>
--- frontend/pages/TasksForm.jsx
+++ frontend/pages/TasksForm.jsx
@@ -5,14 +5,20 @@ const TasksForm = () => {
 
     return (
         <div>
-            <h2>TASK FORM PAGES</h2>
-            <form>
+            <form className="flex flex-col w-full max-w-md px-4 py-8">
+                <label for="description" className="mb-2 text-sm font-medium text-gray-900 dark:text-black">Description</label>
+                <textarea id="description" name="description" className="w-1/2 p-4 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                <label for="start_time" className="mb-2 mt-4 text-sm font-medium text-gray-900 dark:text-black">Start time</label>
+                <input type="time" id="start_time" name="start_time" className="w-1/2 p-4 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                <label for="end_time" className="mb-2 mt-4 text-sm font-medium text-gray-900 dark:texblack">End time</label>
+                <input type="time" id="end_time" name="end_time" className="w-1/2 p-4 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 <Link href="/">
-                    <button type="button">Go to Tasks Chart</button>
+                    <button type="submit" className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Save</button>
                 </Link>
             </form>
         </div>
-
     )
 }
 
