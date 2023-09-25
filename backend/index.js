a9f3f7f732cd68db2fc045eadb667f5c43c5421d
Alex Rigoberto <71194524+alrigo123@users.noreply.github.com>
2023-09-25 07:57:00 -0500

    d

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { router } from './routes/task.routes.js';
+import  router from './routes/task.routes.js';
 
 const app = express();
 
